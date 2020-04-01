e2cb54a571013ca52f07fac8aee4e593d5ad5afd
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-01 17:10:28 +0200

    lists

--- proofs/list.v
+++ proofs/list.v
@@ -83,20 +83,97 @@ Proof.
       reflexivity.
 Qed.
 
+Check (forall xs ,length xs > 0).
 
-      
-
-
-
-
-
-      
-       
+Theorem empty_list_length_ngtz : forall (A : Type), ~ @length A Null > 0.
+Proof.
+    intros A.
+    simpl (length Null).
+    unfold not.
+    intros H.
+    inversion H.
+Qed.
     
+Definition list_head (A : Type) (xs : list A) : length xs > 0 -> A :=
+    match xs with
+    | Cons x _ => fun _ => x
+    | Null => fun pf => match empty_list_length_ngtz A pf with end
+end.
 
-    
+Arguments list_head [A].
 
-    
-    
-    
-    
+Definition list_tail (A : Type) (xs : list A) : length xs > 0 -> list A :=
+    match xs with
+    | Cons _ xs => fun _ => xs
+    | Null => fun pf => match empty_list_length_ngtz A pf with end
+end.
+
+Arguments list_tail [A].
+
+Fixpoint reverse_aux (A : Type) (xs : list A) (acc : list A) : list A :=
+    match xs with 
+    | Null => acc
+    | Cons x xs => reverse_aux A xs (Cons x acc)
+end.
+
+Arguments reverse_aux [A].
+
+Fixpoint reverse (A : Type) (xs : list A) : list A := 
+    reverse_aux xs Null.
+
+Arguments reverse [A].
+
+Definition singleton (A : Type) (x : A) : list A := Cons x Null.
+
+Arguments singleton [A].
+
+Fixpoint app_reverse (A : Type) (xs : list A) : list A :=
+    match xs with
+    | Null => Null
+    | Cons x xs => append (app_reverse A xs) (singleton x)
+end.
+
+Arguments app_reverse [A].
+
+Theorem append_assoc : forall (A : Type) (zs ys xs : list A),
+    append xs (append ys zs) = append (append xs ys) zs.
+Proof.
+    intros A zs ys.
+    induction xs.
+    + simpl.
+      reflexivity.
+    + simpl append.
+      rewrite IHxs.
+      reflexivity.
+Qed.
+
+Theorem append_null_right : forall (A : Type) (xs : list A),
+    append xs Null = xs.
+Proof.
+    intros A.
+    induction xs.
+    + simpl (append Null Null).
+      reflexivity.
+    + simpl (append (Cons a xs) Null).
+      rewrite IHxs.
+      reflexivity.
+Qed.
+
+Theorem reverse_append : forall (A : Type) (ys xs : list A),
+    app_reverse (append xs ys) = append (app_reverse ys) (app_reverse xs).
+Proof.
+    intros A ys.
+    induction xs.
+    + simpl (append Null ys).
+      simpl (app_reverse Null).
+      simpl (append (app_reverse ys) Null).
+      rewrite append_null_right.
+      reflexivity.
+    + replace (app_reverse (append (Cons a xs) ys))
+        with (app_reverse (Cons a (append xs ys)))
+        by auto.
+      simpl app_reverse.
+      rewrite append_assoc.
+      rewrite IHxs.
+      reflexivity.
+Qed.
